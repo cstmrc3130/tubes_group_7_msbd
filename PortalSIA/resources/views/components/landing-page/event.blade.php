b2bbcc28dac496a1a8b661776506cc584a98914d
Recindy Saragih <nataliarecindy@gmail.com>
2022-11-23 19:31:35 +0700

    sikit

---
+++ PortalSIA/resources/views/components/landing-page/event.blade.php
@@ -0,0 +1,38 @@
+<div id="carouselExampleIndicators3" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators3" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators3" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators3" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <img class="img-fluid" src="../../assets/images/mts1.jpg" alt="First slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3 class="text-white">MTsN 1 Rantau Prapat</h3>
+                <p>JL.Letda Sujono LK II - Kec : Bajenis - Kel : Teluk Karang</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="img-fluid" src="../../assets/images/event.jpg" alt="Second slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3 class="text-white">Second title goes here</h3>
+                <p>this is the subcontent you can use this</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="img-fluid" src="../../assets/images/mts.jpg" alt="Third slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3 class="text-white">Third title goes here</h3>
+                <p>this is the subcontent you can use this</p>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators3" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators3" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
